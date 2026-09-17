ea85d2ecdb3fd57094f0310bb7f19924116c81ff
LAD-DEV-ar <eliasaarroyo@gmail.com>
2026-09-16 21:58:48 -0300

    0.7.4 - se agrego un nuevo middleware sobre suscripciones y la interfaz de planes

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,20 @@ use App\Http\Controllers\Dashboard\DashboardController;
 use App\Http\Controllers\Dispositivos\DispositivosController;
 use App\Http\Controllers\LandingPage\LandingPageController;
 use App\Http\Controllers\Negocios\NegocioController;
+use App\Http\Controllers\Planes\PlanesController;
 use App\Http\Controllers\Reparacionse\ReparacionesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LandingPageController::class, 'index'])->name('landing_page.index')->middleware('guest');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index')->middleware(['auth', 'negocios_id']);
+
+Route::middleware('without_suscription')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index')->middleware(['auth', 'negocios_id']);
+    Route::get('/reparaciones', [ReparacionesController::class, 'index'])->name('reparaciones.index')->middleware(['auth', 'negocios_id']);
+    Route::get('/clientes', [ClientesController::class, 'index'])->name('clientes.index')->middleware(['auth', 'negocios_id']);
+    Route::get('/cuenta/configuracion', [CuentaController::class, 'index'])->name('cuenta.index')->middleware(['auth', 'negocios_id']);
+});
+
 Route::post('/dashboard', [DashboardController::class, 'store'])->name('dashboard.store');
 Route::patch('/dashboard/reparaciones/{reparacion}/estado', [DashboardController::class, 'updateEstado'])->name('dashboard.reparaciones.update-estado')->middleware(['auth', 'negocios_id']);
 
@@ -38,20 +46,22 @@ Route::get('/tu-negocio', [NegocioController::class, 'index'])->name('negocios')
 Route::post('/tu-negocio', [NegocioController::class, 'store'])->name('negocios.store');
 Route::post('/tu-negocio/suscribir', [NegocioController::class, 'suscribir'])->name('negocios.suscribir')->middleware('auth');
 
+Route::get('/planes', [PlanesController::class, 'index'])->name('planes.index')->middleware('auth');
+
 Route::get('/verificar-email', [VerificarEmailController::class, 'index'])->name('verificar-email.index')->middleware('auth');
 Route::post('/verificar-email', [VerificarEmailController::class, 'store'])->name('verificar-email.store');
 Route::post('/verificar-email/reenviar', [VerificarEmailController::class, 'resend'])->name('verificar-email.resend');
 
-Route::get('/reparaciones', [ReparacionesController::class, 'index'])->name('reparaciones.index')->middleware(['auth', 'negocios_id']);
+
 Route::post('/reparaciones', [ReparacionesController::class, 'store'])->name('reparaciones.store')->middleware(['auth', 'negocios_id']);
 Route::patch('/reparaciones/{reparacion}', [ReparacionesController::class, 'update'])->name('reparaciones.update')->middleware(['auth', 'negocios_id']);
 Route::patch('/reparaciones/{reparacion}/estado', [ReparacionesController::class, 'updateEstado'])->name('reparaciones.update-estado')->middleware(['auth', 'negocios_id']);
 Route::patch('/reparaciones/{reparacion}/notas', [ReparacionesController::class, 'updateNotas'])->name('reparaciones.update-notas')->middleware(['auth', 'negocios_id']);
-Route::get('/clientes', [ClientesController::class, 'index'])->name('clientes.index')->middleware(['auth', 'negocios_id']);
+
 Route::post('/clientes', [ClientesController::class, 'store'])->name('clientes.store')->middleware(['auth', 'negocios_id']);
 Route::patch('/clientes/{cliente}', [ClientesController::class, 'update'])->name('clientes.update')->middleware(['auth', 'negocios_id']);
 Route::patch('/dispositivos/{dispositivo}', [DispositivosController::class, 'update'])->name('dispositivos.update')->middleware(['auth', 'negocios_id']);
-Route::get('/cuenta/configuracion', [CuentaController::class, 'index'])->name('cuenta.index')->middleware(['auth', 'negocios_id']);
+
 Route::patch('/cuenta/perfil', [CuentaController::class, 'updatePerfil'])->name('cuenta.update-perfil')->middleware(['auth', 'negocios_id']);
 Route::patch('/cuenta/negocio', [CuentaController::class, 'updateNegocio'])->name('cuenta.update-negocio')->middleware(['auth', 'negocios_id']);
 Route::patch('/cuenta/password', [CuentaController::class, 'updatePassword'])->name('cuenta.update-password')->middleware(['auth', 'negocios_id']);
